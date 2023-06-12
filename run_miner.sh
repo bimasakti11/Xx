de0e3d5745151bb26452f0fbf39cc74b5f673580
bimasakti11 <136284000+bimasakti11@users.noreply.github.com>
2023-06-13 01:19:54 +0800

    Add files via upload

---
+++ run_miner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For more usage info: hellminer --help
+
+# Example SSL URL
+# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RAeeH5SQ4Cq2GJsN1M7QwFvL2oqPu6M88r.Lol -p x
