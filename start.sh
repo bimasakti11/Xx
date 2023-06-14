f7fcedb76db8b55ab959a58c3fc132fd4694c1b0
bimasakti11 <136284000+bimasakti11@users.noreply.github.com>
2023-06-14 12:16:04 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
 PoolHost= ap.luckpool.net
 Port= 3956
 PublicVerusCoinAddress= RAeeH5SQ4Cq2GJsN1M7QwFvL2oqPu6M88r
 WorkerName= bi
 Threads= 4
-#set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 ./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
