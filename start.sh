4c2afe756023de8d23dc4d5f47636e9b79a9fe8b
bimasakti11 <136284000+bimasakti11@users.noreply.github.com>
2023-06-14 11:10:42 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-PoolHost= "ap.luckpool.net"
-Port= "3956"
-PublicVerusCoinAddress= "RAeeH5SQ4Cq2GJsN1M7QwFvL2oqPu6M88r"
-WorkerName= "lol"
-Threads= "4"
+PoolHost= stratum+tcp://ap.luckpool.net
+Port= 3956
+PublicVerusCoinAddress= RAeeH5SQ4Cq2GJsN1M7QwFvL2oqPu6M88r
+WorkerName= lol
+Threads= 4
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
