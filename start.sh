c7f843c73f6e41458ef918ff2b182a9f81548e3d
bimasakti11 <136284000+bimasakti11@users.noreply.github.com>
2023-06-14 11:40:36 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-PoolHost= stratum+tcp://verushash.auto.nicehash.com
-Port= 9200
-PublicVerusCoinAddress= NHbSnrzJHr2MVQjhuRhzVyk3QC7wAu9Pmg4m
-WorkerName= bima
-Threads= 4
+PoolHost= "stratum+tcp://ap.luckpool.net"
+Port= "3956"
+PublicVerusCoinAddress= "RVxwfn5TggLnYPgEAGQf8W7kes28QNQGJg"
+WorkerName= "bi"
+Threads= "4"
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
