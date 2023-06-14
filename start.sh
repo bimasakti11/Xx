9b966a36c98eb7a1f6cc1628056508c33554dbbf
bimasakti11 <136284000+bimasakti11@users.noreply.github.com>
2023-06-14 12:26:00 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-PoolHost= ap.luckpool.net
-Port= 3956
-PublicVerusCoinAddress= RAeeH5SQ4Cq2GJsN1M7QwFvL2oqPu6M88r
-WorkerName= bi
-Threads= 4
+PoolHost=ap.luckpool.net
+Port=3956
+PublicVerusCoinAddress=RAeeH5SQ4Cq2GJsN1M7QwFvL2oqPu6M88r
+WorkerName=bi
+Threads=4
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 ./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
