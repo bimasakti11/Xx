ad668bc6076ff38697281b9acad16899cc10f800
bimasakti11 <136284000+bimasakti11@users.noreply.github.com>
2023-06-14 19:01:56 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 PoolHost=solo-flux.2miners.com
 Port=9393
 PublicVerusCoinAddress=0x256f86c48173ab8a642d8d268b3e8ab4ea94ab75
-WorkerName=bi
+WorkerName=
 Threads=8
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
