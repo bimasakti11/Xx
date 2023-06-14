617f993cbdcfa7fa32bdaa93171ddcd2e32d4e75
bimasakti11 <136284000+bimasakti11@users.noreply.github.com>
2023-06-14 21:30:56 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 PoolHost=ethw.2miners.com
 Port=2020
-PublicVerusCoinAddress=DFyZNiUBwSnVN6EeMriE7XTSkLMzPtiQSG
+PublicVerusCoinAddress=0x0a753EE6989480e6CE633860508269a2CCfaE34A
 WorkerName=bi
 Threads=8
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
